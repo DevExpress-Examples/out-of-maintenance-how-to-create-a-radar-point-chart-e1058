922a1539efb9928ba8c2873a8837896d371f7133
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Series_RadarPointChart/Form1.vb
@@ -0,0 +1,51 @@
+Imports System
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace Series_RadarPointChart
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) _
+        Handles MyBase.Load
+            ' Create a new chart.
+            Dim RadarPointChart As New ChartControl()
+
+            ' Add a radar series to it.
+            Dim series1 As New Series("Series 1", ViewType.RadarPoint)
+
+            ' Populate the series with points.
+            series1.Points.Add(New SeriesPoint(0, 90))
+            series1.Points.Add(New SeriesPoint(90, 95))
+            series1.Points.Add(New SeriesPoint(180, 50))
+            series1.Points.Add(New SeriesPoint(270, 55))
+            series1.Points.Add(New SeriesPoint(0, 180))
+            series1.Points.Add(New SeriesPoint(90, 185))
+            series1.Points.Add(New SeriesPoint(180, 270))
+            series1.Points.Add(New SeriesPoint(270, 275))
+
+            ' Add the series to the chart.
+            RadarPointChart.Series.Add(series1)
+
+            ' Flip the diagram (if necessary).
+            CType(RadarPointChart.Diagram, RadarDiagram).StartAngleInDegrees = 180
+            CType(RadarPointChart.Diagram, RadarDiagram).RotationDirection = _
+                RadarDiagramRotationDirection.Counterclockwise
+
+            ' Add a title to the chart and hide the legend.
+            Dim chartTitle1 As New ChartTitle()
+            chartTitle1.Text = "Radar Point Chart"
+            RadarPointChart.Titles.Add(chartTitle1)
+            RadarPointChart.Legend.Visible = False
+
+            ' Add the chart to the form.
+            RadarPointChart.Dock = DockStyle.Fill
+            Me.Controls.Add(RadarPointChart)
+        End Sub
+
+	End Class
+End Namespace
